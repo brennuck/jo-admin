232318777ad7db7751747b2ebc8071838074d137
brennuck <brennuck@gmail.com>
2023-03-06 11:54:57 -0700

    working perfectly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,101 @@
-import "./App.css";
+import React from "react";
+import { Select, message, Form, Input, Button } from "antd";
+import "./index.css";
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    const baseUrl = "https://frozen-dawn-71816.herokuapp.com";
+
+    const updateCountdown = async (value) => {
+        await fetch(`${baseUrl}/countdown/update`, {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                coutdown: value,
+            }),
+        })
+            .then(() => {
+                message.success("Successfully changed countdown");
+            })
+            .catch(() => {
+                message.error("Failed to update countdown");
+            });
+    };
+
+    const addUpdate = async ({ update, color }) => {
+        await fetch(`${baseUrl}/updates`, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                update,
+                color,
+            }),
+        })
+            .then(
+                (resp) =>
+                    resp.status === 200 &&
+                    message.success("Successfully added update")
+            )
+            .catch(() => message.error("Failed to add update"));
+    };
+
+    return (
+        <div className="container">
+            <Form
+                name="updatez"
+                onFinish={({ color, update }) => addUpdate({ update, color })}
+                // onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Update"
+                    name="update"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input the update!",
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Color"
+                    name="color"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input the color!",
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+            <Select
+                placeholder="Countdown"
+                onChange={(value) => updateCountdown(value)}
+                options={[
+                    {
+                        value: true,
+                        label: "Keep counting",
+                    },
+                    {
+                        value: false,
+                        label: "Remove countdown",
+                    },
+                ]}
+            />
+        </div>
+    );
+};
 
 export default App;
